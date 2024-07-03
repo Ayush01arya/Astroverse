a26fb6206384a091f4d4fc2582a8b2a07b80001e
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2024-07-03 14:57:46 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+import Chatbot from '../components/Chatbot';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Company response will be showed here </h1>
+      <Chatbot />
+    </div>
+  );
+};
+
+export default Home;
